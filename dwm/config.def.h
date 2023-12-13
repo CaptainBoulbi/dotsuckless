2758f3a124d97840d289462873698079f4daefaa
cptbbVM <you@example.com>
2023-12-13 12:17:18 +0100

    dmenu bar at bottom

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#A0E418";
 static const char col_gray2[]       = "#E5F9BD";
 static const char col_gray3[]       = "#505250";
-static const char col_gray4[]       = "#505250";
+static const char col_gray4[]       = "#E5F9BD";
 static const char col_cyan[]        = "#7FB414";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
