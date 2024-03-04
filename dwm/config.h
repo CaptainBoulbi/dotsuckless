fe0b08020c9ff400ed2db8cea2adf558ba65f5ac
_N3m0 <n3m0o7@hotmail.com>
2024-03-04 11:52:00 +0100

    map key for power lockscreen

--- dwm/config.h
+++ dwm/config.h
@@ -65,7 +65,12 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+
 static const Key keys[] = {
+    /* custom */
+	//{ 0,   XF86XK_PowerOff,    spawn,  SHCMD("power") },
+	{ MODKEY|ShiftMask, XK_p,    spawn,  SHCMD("power") },
+	{ MODKEY,           XK_l,   spawn,  SHCMD("lockscreen") },
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,   			XK_s,	   spawn,          {.v = termcmd } },
